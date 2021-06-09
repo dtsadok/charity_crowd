8c9931a941caf1818a8a767b13cdd185f8f03856
Daniel <daniel@debian.home>
2021-06-09 13:10:50 -0400

    Updated default DB username/password

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :charity_crowd, CharityCrowd.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "charity_crowd",
+  password: "charity_crowd",
   database: "charity_crowd_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
