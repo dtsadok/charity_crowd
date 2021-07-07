27218cf16423972bb90c499b3b6b639373a6754e
Daniel <daniel@debian.home>
2021-07-07 03:00:20 -0400

    Added starting balance to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,5 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+CharityCrowd.Repo.insert!(%CharityCrowd.Endowment.Balance{amount_cents: 1000_00})
